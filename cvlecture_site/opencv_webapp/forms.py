f82628ab86f42640b4d3b15e0029fe49f00b64d7
cvlecture <rohujin97@gmail.com>
2019-05-28 21:09:42 -0700

    modified:   cvlecture_site/cvlecture_site/urls.py 	modified:   cvlecture_site/opencv_webapp/forms.py 	modified:   cvlecture_site/opencv_webapp/models.py

--- cvlecture_site/opencv_webapp/forms.py
+++ cvlecture_site/opencv_webapp/forms.py
@@ -1,14 +1,12 @@
 from django import forms
 from .models import ImageUploadModel
-<<<<<<< HEAD
+
 from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
     class Meta:
         model=User
         fields=['username', 'email','password']
-=======
->>>>>>> bcde6757cd3e8f462d9e3530d628140754152c24
 
 class UploadImageForm(forms.Form):
     title = forms.CharField(max_length=50)
@@ -18,8 +16,5 @@ class UploadImageForm(forms.Form):
 class ImageUploadForm(forms.ModelForm):
     class Meta:
         model = ImageUploadModel
-<<<<<<< HEAD
         fields = ('description', 'document' )
-=======
         fields = ('description', 'document')
->>>>>>> bcde6757cd3e8f462d9e3530d628140754152c24
--- cvlecture_site/opencv_webapp/models.py
+++ cvlecture_site/opencv_webapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-<<<<<<< HEAD
+
 
 #class ImageUploadModel(models.Model):
 #    description = models.CharField(max_length=255, blank=True)
@@ -12,9 +12,9 @@ class ImageUploadModel(models.Model):
   description = models.CharField(max_length=255, blank=True)
   document = models.ImageField(upload_to='images/%Y/%m/%d')
   uploaded_at = models.DateTimeField(auto_now_add=True)
-=======
+
 class ImageUploadModel(models.Model):
   description = models.CharField(max_length=255, blank=True)
   document = models.ImageField(upload_to='images/%Y/%m/%d')
   uploaded_at = models.DateTimeField(auto_now_add=True)
->>>>>>> bcde6757cd3e8f462d9e3530d628140754152c24
+
