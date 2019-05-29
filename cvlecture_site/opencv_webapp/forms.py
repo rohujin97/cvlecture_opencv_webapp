18987cdef64abcb22a68220fc2c737958696c631
subin97 <subin_97@likelion.org>
2019-05-29 20:58:50 +0900

    login

--- cvlecture_site/opencv_webapp/forms.py
+++ cvlecture_site/opencv_webapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import ImageUploadModel
+from django.contrib.auth.models import User
 
 class UploadImageForm(forms.Form):
     title = forms.CharField(max_length=50)
@@ -10,3 +11,14 @@ class ImageUploadForm(forms.ModelForm):
     class Meta:
         model = ImageUploadModel
         fields = ('description', 'document')
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
--- cvlecture_site/opencv_webapp/views.py
+++ cvlecture_site/opencv_webapp/views.py
@@ -4,10 +4,16 @@ from .forms import UploadImageForm
 from .forms import ImageUploadForm
 from django.conf import settings
 from .opencv_dobj import opencv_dobj
+from .forms import UserForm, LoginForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponse
+from django.template import RequestContext
 
 from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
+
 
 def dobj(request):
     if request.method == 'POST':
@@ -39,3 +45,30 @@ def uimage(request):
   else:
       form = UploadImageForm()
       return render(request, 'opencv_webapp/uimage.html', {'form': form})
+
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            new_user = User.objects.create_user(**form.cleaned_data)
+            login(request, new_user)
+            return redirect('first_view')
+        return render(request, 'opencv_webapp/dobj.html', {'form': form})
+    else:
+        form = UserForm()
+        return render(request, 'opencv_webapp/dobj.html', {'form': form})
+
+def signin(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            login(request, user)
+            return redirect('dobj')
+        else:
+            return HttpResponse('Login Failed')
+    else:
+        form = LoginForm()
+        return render(request, 'opencv_webapp/signin.html', {'form':form})
